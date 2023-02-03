d9ab3d2d9132c63e5de625449f66c43c3b1758e3
Sim <sima.simute1999@gmail.com>
2023-02-03 23:13:55 +0200

    Player Controller 0.0.1

---
+++ Assets/_SIM/PlayerController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float turnSpeed = 25.0f;
+    public float jumpHeight = 2.0f;
+    public Animator anim;
+    
+    private Rigidbody _rb;
+    PlayerInput _input;
+    private bool _isGrounded;
+    
+    Vector2 _movedir;
+    Vector2 _lookdir;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _input = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        _movedir = _input.actions["move"].ReadValue<Vector2>();
+        
+        //if player is on the ground
+        if (_isGrounded)
+        {
+            //if player presses jump
+            if (_input.actions["a"].triggered)
+            {
+                //add force to player
+                _rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            }
+        }
+
+        if (_input.actions["b"].triggered)
+        {
+            Plant();
+            
+            if (anim != null)
+                anim.SetTrigger("action");
+        }
+
+        if (anim != null)
+        {
+            anim.SetBool("grounded", _isGrounded);
+            anim.SetFloat("speed", _movedir.magnitude);
+        }
+        
+    }
+
+    void Plant()
+    {
+        print("Planting");
+    }
+    
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(_movedir.x * speed, _rb.velocity.y, _movedir.y * speed);
+        
+        //rotate smoothly toward look dir
+        Vector3 lookDir = new Vector3(_movedir.x, 0, _movedir.y);
+        if (lookDir != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookDir);
+            Quaternion newRotation = Quaternion.Lerp(_rb.rotation, targetRotation, turnSpeed * Time.deltaTime);
+            _rb.MoveRotation(newRotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.CompareTag("Ground"))
+            _isGrounded = true;
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        if(other.transform.CompareTag("Ground"))
+            _isGrounded = false;
+    }
+}
