28866fef12f4d2b41d9aa3d010697ed549c15bc0
Vik <vik :)>
2023-02-03 23:28:07 +0200

    blob shadow

--- Assets/_VIK/PlaneShadow.cs
+++ Assets/_VIK/PlaneShadow.cs
@@ -1,8 +1,22 @@
 
+using System;
 using UnityEngine;
 
 public class PlaneShadow : MonoBehaviour
 {
-    
-    
+    public Transform target;
+    public float height = 0.1f;
+
+
+    private void Awake()
+    {
+        if(target == null)target = transform.parent;
+    }
+
+
+    private void LateUpdate()
+    {
+        if( target != null)
+            transform.position = new Vector3(target.position.x, height, target.position.z);
+    }
 }
